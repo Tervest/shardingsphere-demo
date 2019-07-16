5bcf8fdf525723d2a09828520f7d1c3d0f58e897
znn <you@example.com>
2019-07-16 11:31:38 +0800

    1.分组

---
+++ sharding-jdbc/src/main/java/top/tervest/shardingsphere/shardingjdbc/entity/po/OrderCountDo.java
@@ -0,0 +1,13 @@
+package top.tervest.shardingsphere.shardingjdbc.entity.po;
+
+import lombok.Data;
+
+/**
+ * @author ZhangNanNan
+ * @create 2019-07-16 11:09
+ */
+@Data
+public class OrderCountDo {
+    private Integer userId;
+    private Integer count;
+}
